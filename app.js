d871c20ba56ad6c87865aaf84733105dc961d42f
Stjepan <stjepan@Stjepans-MBP.mshome.net>
2021-11-07 10:42:17 +0100

    Set up heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 const { auth, requiresAuth } = require('express-openid-connect'); 
-const port = 4080;
+const port = process.env.PORT || 4080;
 
 app.use(express.json())
 users = {}
@@ -19,7 +19,7 @@ const config = {
   authRequired : false,
   idpLogout : true, //login not only from the app, but also from identity provider
   secret: process.env.SECRET,
-  baseURL: `https://localhost:${port}`,
+  baseURL: process.env.APP_URL || `https://localhost:${port}`,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: 'https://dev-528z288p.eu.auth0.com',
   clientSecret: process.env.CLIENT_SECRET,
@@ -39,7 +39,8 @@ app.get('/',  function (req, res) {
         req.user.name = req.oidc.user.name;
         req.user.timestamp = req.oidc.user.updated_at;
     }
-    res.render('index', {user : req.user, userString: JSON.stringify(req.user)});
+    let appUrl = process.env.APP_URL || `https://localhost:${port}`
+    res.render('index', {user : req.user, userString: JSON.stringify(req.user), appUrl});
 });
 
 app.get('/private', requiresAuth(), function (req, res) {       
@@ -47,7 +48,7 @@ app.get('/private', requiresAuth(), function (req, res) {
     recentUsers = Object.values(users)
     recentUsers.sort((a,b) => (a.timestamp > b.timestamp) ? 1 : ((b.timestamp > a.timestamp) ? -1 : 0))
     recentUsers = JSON.stringify(recentUsers.slice(0, 5))
-    console.log(recentUsers)
+    console.log(JSON.parse(recentUsers))
     res.render('private', {user, recentUsers});
 });
 
@@ -61,7 +62,6 @@ app.get("/sign-up", (req, res) => {
 });
 
 app.post('/store', (req, res) => {
-  console.log(req.body)
   users[req.body.user] = {
     user: req.body.user,
     timestamp: req.body.timestamp,
@@ -70,10 +70,14 @@ app.post('/store', (req, res) => {
   }
 });
 
-https.createServer({
+if(process.env.PORT) {
+
+} else {
+  https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
   }, app)
   .listen(port, function () {
     console.log(`Server running at https://localhost:${port}/`);
   });
+}
