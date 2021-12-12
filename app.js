ec4823a8766523ce59b9aabddf59c6501459a4e1
Stjepan <stjepan@Stjepans-MBP.mshome.net>
2021-12-12 21:33:31 +0100

    lab2 changes

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ var fs = require('fs')
 var https = require('https')
 const app = express();
 
-const CryptoJS = require('crypto-js')
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
-const { auth, requiresAuth } = require('express-openid-connect'); 
+const { auth, requiresAuth } = require('express-openid-connect');
 const port = process.env.PORT || 4080;
 
 app.use(express.json())
@@ -38,64 +36,62 @@ var users = {
 };
 const appUrl = process.env.APP_URL || `https://localhost:${port}/`
 
-const config = { 
-  authRequired : false,
-  idpLogout : true, //login not only from the app, but also from identity provider
+const config = {
+  authRequired: false,
+  idpLogout: true, //login not only from the app, but also from identity provider
   secret: process.env.SECRET,
   baseURL: process.env.APP_URL || `https://localhost:${port}`,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: 'https://dev-528z288p.eu.auth0.com',
   clientSecret: process.env.CLIENT_SECRET,
   authorizationParams: {
-    response_type: 'code' ,
+    response_type: 'code',
     //scope: "openid profile email"   
-   },
+  },
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.get('/',  function (req, res) {
-    req.user = {
-        isAuthenticated : req.oidc.isAuthenticated()
-    };
-    if (req.user.isAuthenticated) {
-        req.user.name = req.oidc.user.name;
-        req.user.timestamp = req.oidc.user.updated_at;
-    }
-    res.render('index', {user : req.user, userString: JSON.stringify(req.user), appUrl});
+app.get('/', function (req, res) {
+  req.user = {
+    isAuthenticated: req.oidc.isAuthenticated()
+  };
+  if (req.user.isAuthenticated) {
+    req.user.name = req.oidc.user.name;
+    req.user.timestamp = req.oidc.user.updated_at;
+  }
+  res.render('index', { user: req.user, userString: JSON.stringify(req.user), appUrl });
 });
 
-app.get('/sensitive-data', requiresAuth(), function (req, res) {       
-    const user = JSON.stringify(req.oidc.user)
-    let textsActual = []
-    if(safeMode[0]) {
-      texts.forEach(t => {
-
-      })
-    } else {
-      textsActual = texts
-    }
-    res.render('sensitive-data', {safeMode: safeMode[0], user, appUrl, database: JSON.stringify(textsActual), texts: JSON.stringify(texts)});
+app.get('/sensitive-data', requiresAuth(), function (req, res) {
+  const user = JSON.stringify(req.oidc.user)
+  let textsActual = []
+  if (safeMode[0]) {
+    textsActual = "This was supposed to be encrypted, to show how sensitive data should actually be stored. But, I had problems with crypto libraries in js and I didn't implement it."
+  } else {
+    textsActual = texts
+  }
+  res.render('sensitive-data', { safeMode: safeMode[0], user, appUrl, database: JSON.stringify(textsActual), texts: JSON.stringify(texts) });
 });
 
-app.get('/access-control', requiresAuth(), function (req, res) {       
-  
+app.get('/access-control', requiresAuth(), function (req, res) {
+
   let reqUser = req.query.user
-  if(safeMode[2]) {
-    if(reqUser === undefined)
+  if (safeMode[2]) {
+    if (reqUser === undefined)
       user = `User ${reqUser} doesn't exist`
-    else if(reqUser !== req.oidc.user.name)
+    else if (reqUser !== req.oidc.user.name)
       user = `You're not authorized to view this page`
     else
       var user = JSON.stringify(req.oidc.user)
-    res.render('access-control', {user, appUrl, safeMode: safeMode[2]})
+    res.render('access-control', { user, appUrl, safeMode: safeMode[2] })
 
   } else {
     user = users[reqUser]
-    if(user === undefined)
+    if (user === undefined)
       user = `User ${reqUser} doesn't exist`
-    res.render('access-control', {user, appUrl, safeMode: safeMode[2]})
+    res.render('access-control', { user, appUrl, safeMode: safeMode[2] })
   }
 });
 
@@ -108,7 +104,7 @@ app.get("/login", (req, res) => {
 app.get("/sign-up", (req, res) => {
   res.oidc.login({
     returnTo: '/',
-    authorizationParams: {      
+    authorizationParams: {
       screen_hint: "signup",
     },
   });
@@ -124,23 +120,16 @@ app.post('/toggle-safe-mode', (req, res) => {
 app.post('/submit-text', (req, res) => {
   let text = req.body.text
 
-  if(safeMode[0]) {
-    var encrypted = CryptoJS.AES.encrypt(text, "Test", {
-      format: JsonFormatter
-    });
-    console.log(encrypted.toString())
-    var decrypted = CryptoJS.AES.decrypt(encrypted, "Test", {
-      format: JsonFormatter
-    });
-    console.log(decrypted.toString())
+  if (safeMode[0]) {
+    texts.push(text)
   } else {
     texts.push(text)
-  } 
+  }
   res.status(200)
   res.send()
 });
 
-if(process.env.PORT) {
+if (process.env.PORT) {
   app.listen(port, () => {
     console.log(`Server running at ${process.env.APP_URL}:${port}/`);
   })
@@ -149,47 +138,7 @@ if(process.env.PORT) {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
   }, app)
-  .listen(port, function () {
-    console.log(`Server running at https://localhost:${port}/`);
-  });
-}
-
-var JsonFormatter = {
-  stringify: function(cipherParams) {
-    // create json object with ciphertext
-    var jsonObj = { ct: cipherParams.cipherText.toString(CryptoJS.enc.Base64)}
-
-    // optionally add iv or salt
-    if (cipherParams.iv) {
-      jsonObj.iv = cipherParams.iv.toString();
-    }
-
-    if (cipherParams.salt) {
-      jsonObj.s = cipherParams.salt.toString();
-    }
-
-    // stringify json object
-    return JSON.stringify(jsonObj)
-  },
-  parse: function(jsonStr) {ž
-    // parse json string
-    var jsonObj = JSON.parse(jsonStr);
-    
-    // extract ciphertext from json object, and create cipher params object
-    var cipherParams = CryptoJS.lib.CipherParams.create({
-      ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
+    .listen(port, function () {
+      console.log(`Server running at https://localhost:${port}/`);
     });
-
-    // optionally extract iv or salt
-
-    if (jsonObj.iv) {
-      cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv);
-    }
-
-    if (jsonObj.s) {
-      cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s);
-    }
-
-    return cipherParams;
-  }
 }
